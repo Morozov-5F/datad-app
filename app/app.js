fcd3fb1424e4e00c416dfd6114802d8a3ac57d85
Evgeniy Morozov <zhe3145@yandex.ru>
2016-10-08 15:06:51 +0300

    Added routing and templates

--- app/app.js
+++ app/app.js
@@ -11,3 +11,73 @@ app.run(function ($ionicPlatform) {
         }
     });
 });
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      .state('menu.simpleSearch', {
+    url: '/search',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/simpleSearch.html',
+        // controller: 'simpleSearchCtrl'
+      }
+    }
+  })
+
+  .state('menu.advancedSearch', {
+    url: '/adv_serach',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/advancedSearch.html',
+        // controller: 'advancedSearchCtrl'
+      }
+    }
+  })
+
+  .state('menu.profile', {
+    url: '/profile',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/profile.html',
+        // controller: 'profileCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu',
+    templateUrl: 'templates/menu.html',
+    // controller: 'menuCtrl'
+  })
+
+  .state('menu.searchResults', {
+    url: '/search_results',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/searchResults.html',
+        // controller: 'searchResultsCtrl'
+      }
+    }
+  })
+
+  .state('menu.information', {
+    url: '/advanced_info',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/information.html',
+        // controller: 'informationCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu/search')
+
+  
+
+});
