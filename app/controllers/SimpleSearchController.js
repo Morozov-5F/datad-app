66f942da9e80f71f4e2de0b2fe6e9b4a352f161e
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-10-08 19:07:42 +0300

    Иконки на экране поиска

--- app/controllers/SimpleSearchController.js
+++ app/controllers/SimpleSearchController.js
@@ -17,13 +17,17 @@ app.controller('SimpleSearchController', function ($scope, $log) {
     ];
     $scope.services =  [
         {
-            name: "VK",
+            name: "vk",
             checked: true,
         },
         {
-            name: "YouTube",
+            name: "youtube",
             checked: true,
         },
+        {
+            name: "instagram",
+            checked: false,
+        }        
     ];
     $scope.searchFields = {
         price: 1000,
@@ -39,4 +43,17 @@ app.controller('SimpleSearchController', function ($scope, $log) {
         });
     }, true);
     $scope.getFieldsString = () => JSON.stringify($scope.searchFields);
+    $scope.getServiceIconClass = (service) => {
+        let classes = ['search-service-icon'];
+        if (service.checked)
+            classes.push('search-service-icon-checked')
+        else
+            classes.push('search-service-icon-unchecked')
+
+        return classes;
+    };
+    $scope.toggleServiceIcon = (index) => {
+        $scope.services[index].checked = !$scope.services[index].checked;
+
+    };
 });
