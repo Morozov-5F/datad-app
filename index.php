80a814365c449cfd0f665e398a7c687ecf783516
aristovz <nbapavel@me.com>
2016-10-08 18:16:14 +0300

    v0.1

--- index.php
+++ index.php
@@ -5,16 +5,17 @@
 	//errors
 	//1 - не правильный запрос (нету api/)
 	//2 - Bad request
-	//3 - ошибка добавления в базу данных
 	//4 - нету обязательных параметров
+	// -1 - метод не найден
+	// 5 - пользователь не найден
+	// 6 - нету параметров
 	
 	//echo '<pre>';
 	//print_r(json_encode($_SERVER['REQUEST_METHOD']));
 	//echo json_encode($_POST);
 	//print_r($_GET['q']);
 	
-		
-
+	
 	preg_match("#api/(.*)#", $_GET['q'], $matches);
 	if (empty($matches)) { echo 'error 1'; exit; }
 	
@@ -24,8 +25,24 @@
 			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				$data = json_decode(file_get_contents("php://input"), true);
 				
+				if (!isset($data['name']) || !isset($data['email']) || !isset($data['password'])) {
+					$res['error'] = 'BAD_PARAMETERS';
+					$res['error_code'] = 4; 
+					
+					header('Content-Type: application/json; charset=utf-8');
+					print_r(json_encode($res));
+					
+					exit;
+				}
+				
 				$id = insert('users', ['name' => $data['name'], 'email' => $data['email'], 'password' => $data['password']]);
-				if ($id == 0) { echo 'error: 3'; }
+				if ($id == 0) {
+					$res['error'] = 'ERROR_ADD_TO_DB';
+					$res['error_code'] = 6; 
+					
+					header('Content-Type: application/json; charset=utf-8');
+					print_r(json_encode($res));
+				}
 				else {
 					echo 'sucess registration';	
 				}
@@ -34,7 +51,7 @@
 			break;
 			
 		# Метод для получения списка пользователей
-		case 'api/users.get': 
+		case 'api/users.get':
 			if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 				$limit = 100; //дефолтный лимит
 				$after = 0;
@@ -49,12 +66,9 @@
 				
 				$users = select("SELECT * FROM `users` LIMIT ".$after.", ".$limit);
 				
-				if ($users) {	
-					$res['count'] = count($users);
-					$res['users'] = $users;
-				}
-				else { $res['count'] = 0; }
-				
+				$res['count'] = count($users);
+				$res['users'] = $users;
+			
 				header('Content-Type: application/json; charset=utf-8');
 				print_r(json_encode($res));
 		    }
@@ -64,23 +78,90 @@
 		# Метод для получения пользователя по id
 		case 'api/users.getByID': 
 			if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-				$ids = [];
-				
-				if (!isset($_GET['ids'])) { echo 'error 4'; exit; }
+				if (!isset($_GET['ids'])) { $res['error'] = 'BAD_PARAMETERS';
+					$res['error_code'] = 4; 
+					
+					header('Content-Type: application/json; charset=utf-8');
+					print_r(json_encode($res));
+					exit;
+				}
 
 				$users = select("SELECT * FROM `users` WHERE id IN (".$_GET['ids'].")");
 			
 				$res['count'] = count($users);
-				$res['users'] = $users ?? 123;
-
-
+				$res['users'] = $users;
+				
 				header('Content-Type: application/json; charset=utf-8');
 				print_r(json_encode($res));
 		    }
 		    else { echo 'error: 2'; }
 			break;	
 			
-		default: echo 111;
+		# Проверка email на уникальность
+		case 'api/checkAccount': 
+			if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+				
+				if (!isset($_GET['email'])) { echo 'error 4'; exit; }
+
+				$user = selectrow("SELECT * FROM `users` WHERE email LIKE '".$_GET['email']."' LIMIT 1");
+			
+				if (!$user) 
+				{ 
+					$res['error'] = 'USER_NOT_FOUND';
+					$res['error_code'] = 5;
+				}
+				else { $res['user'] = $user; }
+				
+				header('Content-Type: application/json; charset=utf-8');
+				print_r(json_encode($res));
+		    }
+		    else { echo 'error: 2'; }
+			break;
+		
+		# Логин
+		case 'api/login': 
+			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+				$data = json_decode(file_get_contents("php://input"), true);
+				
+				if (!isset($data['email']) || !isset($data['password'])) {
+					$res['error'] = 'BAD_PARAMETERS';
+					$res['error_code'] = 4; 
+					
+					header('Content-Type: application/json; charset=utf-8');
+					print_r(json_encode($res));
+					
+					exit;
+				}
+				
+				
+				
+				$curl = new CurlBasic();
+			 	//$curl->url('http://nk5.ru/api/checkAccount?email='.$data['email']);
+			 	$curl->url('google.com');
+			
+				$page = $curl->makerequest();
+				print_r($page);
+				
+				/*
+$user = select('users', ['name' => $data['name'], 'email' => $data['email'], 'password' => $data['password']]);
+				if ($id == 0) {
+					$res['error'] = 'ERROR_ADD_TO_DB';
+					$res['error_code'] = 6; 
+					
+					header('Content-Type: application/json; charset=utf-8');w
+					print_r(json_encode($res));
+				}
+				else {
+					echo 'sucess registration';	
+				}
+*/
+
+		    }
+		    else { echo 'error: 2'; }
+			
+			break;	
+			
+		default: echo 'error: -1';
 	}
 	
 	
