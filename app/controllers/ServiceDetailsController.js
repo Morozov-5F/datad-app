8e23fb2bf25ac954dda4d110d6db610bad83098c
Evgeniy Morozov <zhe3145@yandex.ru>
2016-10-09 04:25:40 +0300

    Added fancy subscribers counters

--- app/controllers/ServiceDetailsController.js
+++ app/controllers/ServiceDetailsController.js
@@ -1,6 +1,23 @@
 const app = angular.module('app');
 
 app.controller('ServiceDetailsController', function ($scope, $stateParams, Providers) {
+    $scope.cropValue = (val) => {
+        let res = '';
+        if (val >= 1e+6)
+        {
+            res = Math.round(Math.floor(val / 1e+5)) / 10 + 'M';
+        }
+        else if (val >= 1e+3)
+        {
+            res = Math.round(val / 1e+2) / 10 + 'k';
+        }
+        else
+        {
+            res = Math.floor(val);
+        }
+        return res;
+    };
+    
     Providers.getByID($stateParams.id)
         .then((result) => {
             if (result.data && result.data.users && result.data.users[0])
@@ -9,6 +26,7 @@ app.controller('ServiceDetailsController', function ($scope, $stateParams, Provi
             $scope.profile.description = decodeURIComponent($scope.profile.description);
             if ($scope.profile.description.length > 100) 
                 $scope.profile.description = $scope.profile.description.slice(0, 110) + '...';
+            
         });
     $scope.profile = {};
 
