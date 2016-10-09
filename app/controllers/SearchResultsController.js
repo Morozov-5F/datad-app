fa1e8b06caf89bce5c1d0ac145935c90fdc07351
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-10-09 03:56:44 +0300

    Fixed fonts

--- app/controllers/SearchResultsController.js
+++ app/controllers/SearchResultsController.js
@@ -13,13 +13,20 @@ app.controller('SearchResultsController', function ($scope, $stateParams, Servic
         'instagram'
     ];
 
+    $scope.searchResults = [];
+    $scope.totalResultsCount = 0;
     Providers.search(searchParams)
         .then((result) => $scope.searchResults = result.data.users)
         .catch((error) => console.log(error))
         .finally(() => $scope.loading = false);
 
+
+    $scope.$watch('searchResults', () => {
+        $scope.totalResultsCount = $scope.searchResults.length;
+        console.log($scope.searchResults.length, $scope.totalResultsCount);
+    });
+
     let randomPrice = () => (Math.floor(Math.random() * 120) + 25) * 10;
-    $scope.searchResults = [];
     $scope.loading = true;
     $scope.getServiceName = (socialID) => serviceNames[socialID - 1]; 
     $scope.getServiceIconPath = (socialID, size) => 'assets/img/service_icons/' + $scope.getServiceName(socialID) + '-colored.png';
